129996998ed2ec7898a1f0bfcb947e8928e6ec9e
abhay-byte <abhay02delhi@gmail.com>
2026-03-15 10:30:41 +0530

    chore: bump version to 1.4p for Play Store release track

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 5
-        versionName = "1.4"
+        versionName = "1.4p"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
